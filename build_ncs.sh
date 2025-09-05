00ed6ef9ec34ab29211f2f4a60e58199ae132009
Alex Roth <alex@alexroth.me>
2025-09-05 12:30:23 -0400

    Builds, openthread comes up at boot. Don't like the flash wear caused by settings + OpenThread.

---
+++ build_ncs.sh
@@ -0,0 +1,2 @@
+export ZEPHYR_BASE=/home/alex/Downloads/nrf/sdk/v3.1.0/zephyr/
+west build --pristine  -b nrf54l15dk/nrf54l15/cpuapp
--- src/main.c
+++ src/main.c
@@ -9,12 +9,50 @@
 #include <zephyr/drivers/display.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/settings/settings.h>
+
+#include <zephyr/net/openthread.h>
+#include <openthread/thread.h>
 
 LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 /* Define the size of the box */
 #define BOX_WIDTH  40
 #define BOX_HEIGHT 40
+
+
+static void on_thread_state_changed(otChangedFlags flags, void *user_data)
+{
+	if (flags & OT_CHANGED_THREAD_ROLE) {
+		switch (otThreadGetDeviceRole(openthread_get_default_instance())) {
+		case OT_DEVICE_ROLE_CHILD:
+		case OT_DEVICE_ROLE_ROUTER:
+		case OT_DEVICE_ROLE_LEADER:
+			LOG_INF("OpenThread connected");
+			break;
+
+		case OT_DEVICE_ROLE_DISABLED:
+		case OT_DEVICE_ROLE_DETACHED:
+		default:
+			LOG_INF("OpenThread detached");
+			break;
+		}
+	}
+    else if (flags & OT_CHANGED_PARENT_LINK_QUALITY) {
+        otRouterInfo parentInfo;
+        otError err = otThreadGetParentInfo(openthread_get_default_instance(), &parentInfo);
+        if (err != OT_ERROR_NONE) {
+            LOG_INF("Error getting parent status: %d", err);
+        }
+        else {
+            LOG_INF("Parent link quality changed: in: %u out: %u cost: %u", parentInfo.mLinkQualityIn, parentInfo.mLinkQualityOut, parentInfo.mPathCost);
+        }
+        
+    }
+}
+
+static struct openthread_state_changed_callback ot_state_chaged_cb = {
+	.otCallback = on_thread_state_changed};
 
 int main(void)
 {
@@ -38,6 +76,20 @@ int main(void)
     LOG_INF("Current pixel format: %x", caps.current_pixel_format);
     LOG_INF("Current orientation: %d", caps.current_orientation);
 
+    // OT does a setting write every startup to save new network and parent info.
+    // I _really_ don't like this unneccessary flash wear.
+    // 
+
+    // otDatasetParseTlvs -> convert binary dataset into real dataset
+    // otDatasetSetActiveTlvs -> Sets binary dataset as active dataset.
+    //
+    // Can we disable settings integration and just do an `otDatasetSetActiveTlvs` on every boot?
+    // Probably will take a bit longer.
+    // Alternatively, can we disable writing to `kKeyNetworkInfo` / `kKeyParentInfo`?
+
+    openthread_state_changed_callback_register(&ot_state_chaged_cb);
+    LOG_INF("Starting OpenThread!");
+    openthread_run();
 
     /* Define a buffer for our 10x10 pixel box */
     /* NOTE: The buffer format must match the display's native format.
@@ -88,6 +140,7 @@ int main(void)
 	display_blanking_off(display_dev);
 
 	LOG_INF("Black box drawn successfully in the center of the display.");
+    
     
 
     /* The application can now enter a low-power state or do other work */
