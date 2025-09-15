6b4873964f963f5c57b9442d66ef975a19632382
Alex Roth <alex@alexroth.me>
2025-09-15 18:17:33 -0400

    db temp

--- host/src/database.rs
+++ host/src/database.rs
@@ -1,42 +1,29 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use thiserror::Error;
-use async_trait::async_trait;
-use crate::types::{DeviceState, DeviceStateRow};
+use crate::types::{DeviceState};
 
 #[derive(Error, Debug)]
 pub enum DatabaseError {
     #[error("Device not found with ID: {device_id}")]
     DeviceNotFound { device_id: u64 },
 
-    #[error("Database connection error")]
-    ConnectionError(#[from] sqlx::Error),
-
 }
 
 // "database" is a serialization/deserialization layer for DeviceState and DeviceConfig objects.
-// It will use the sqlx library to save and load objects from PostgreSQL.
-#[async_trait]
+// It will use the diesel library to save/load objects from Postgres.
 pub trait Database {
-    async fn get_device_state(&self, device_id: u64) -> Result<DeviceState, DatabaseError>;
-    async fn add_new_device(&self, device_state: DeviceState) -> Result<(), DatabaseError>;
 }
 
 
 pub struct DBImpl {
-    pub pool: Pool<Postgres>
 }
 
 impl DBImpl {
     pub async fn new(connstr: &str) -> Result<Self, anyhow::Error> {
-        let pool = PgPoolOptions::new().max_connections(5).connect(connstr).await?;
-
-        sqlx::migrate!().run(&pool).await?;
-
-        Ok(DBImpl {
-            pool
-        })
+        Err(anyhow::anyhow!("unimplemented"))
     }
 }
 
 
+
+//cargo install diesel_cli --no-default-features --features postgres,postgres-bundled
 // database URL for testing: psql "postgres://epaper:epaper_password@127.0.0.1/epaper"
